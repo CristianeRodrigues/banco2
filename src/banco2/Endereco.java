5e19bb4d9b9d43f5125667a28a9d04e900db3c78
cristiane <cris_csr@yahoo.com.br>
2019-02-26 09:37:40 -0300

    first commit

---
+++ src/banco2/Endereco.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package banco2;
+
+/**
+ *
+ * @author Carlos
+ */
+public class Endereco {
+     String rua;
+    String numero;
+    String bairro;
+    
+     public void setRua(String rua){
+       this.rua = rua; 
+    }
+}
---
+++ src/banco2/Funcionario.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package banco2;
+
+/**
+ *
+ * @author Carlos
+ */
+public class Funcionario {
+
+    int codigo;
+    String função;
+    Endereco end;
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+        System.out.println("código");
+    }
+}
